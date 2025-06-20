84bd9c4853c7cc2d442f4f73b50c0618958f32d7
doni <doniaries@gmail.com>
2025-06-20 10:16:54 +0700

    fdsdfds

--- database/seeders/InformasiSeeder.php
+++ database/seeders/InformasiSeeder.php
@@ -12,10 +12,36 @@ class InformasiSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Team::all()->each(function ($team) {
-            \App\Models\Informasi::factory(4)->create([
-                'team_id' => $team->id,
-            ]);
+        $informasiData = [
+            [
+                'judul' => 'Peningkatan Layanan Publik Digital', 
+                'isi' => 'Dinas Komunikasi dan Informatika (Diskominfo) meluncurkan program peningkatan layanan publik berbasis digital untuk memudahkan akses masyarakat terhadap informasi dan layanan pemerintah. Inisiatif ini mencakup pengembangan aplikasi mobile dan portal web interaktif.',
+                'file' => 'https://example.com/file1.pdf',
+            ],
+            [
+                'judul' => 'Sosialisasi Keamanan Siber untuk UMKM', 
+                'isi' => 'Diskominfo mengadakan sosialisasi keamanan siber bagi pelaku Usaha Mikro, Kecil, dan Menengah (UMKM) di wilayah ini. Tujuannya adalah untuk meningkatkan kesadaran dan kapasitas UMKM dalam menghadapi ancaman siber yang semakin kompleks.',
+                'file' => 'https://example.com/file2.pdf',
+            ],
+            [
+                'judul' => 'Pelatihan Literasi Digital bagi Masyarakat', 
+                'isi' => 'Dalam upaya meningkatkan literasi digital masyarakat, Diskominfo menyelenggarakan serangkaian pelatihan gratis. Materi pelatihan meliputi penggunaan internet sehat, pencegahan hoaks, dan pemanfaatan media sosial secara bijak.',
+                'file' => 'https://example.com/file3.pdf',
+            ],
+            [
+                'judul' => 'Pengembangan Infrastruktur Jaringan Internet', 
+                'isi' => 'Pemerintah daerah melalui Diskominfo terus berupaya memperluas jangkauan dan meningkatkan kualitas infrastruktur jaringan internet di seluruh pelosok daerah. Hal ini diharapkan dapat mendukung pemerataan akses informasi dan ekonomi digital.',
+                'file' => 'https://example.com/file4.pdf',
+            ],
+        ];
+
+        \App\Models\Team::all()->each(function ($team) use ($informasiData) {
+            foreach ($informasiData as $data) {
+                \App\Models\Informasi::create(array_merge($data, [
+                    'team_id' => $team->id,
+                    'slug' => \Illuminate\Support\Str::slug($data['judul']),
+                ]));
+            }
         });
     }
 }
